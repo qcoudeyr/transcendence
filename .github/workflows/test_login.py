09d98e295207067b98ade7fe1ffa94361480a69a
qcoudeyr <qcoudeyr@student.42perpignan.fr>
2024-07-25 19:11:08 +0200

    chore: Remove unnecessary files and directories

--- .github/workflows/test_login.py
+++ .github/workflows/test_login.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+import os
+import signal
 
 # Configure Chrome options
 chrome_options = Options()
@@ -51,4 +53,4 @@ driver.save_screenshot('prometheus_page.png')
 try:
     driver.quit()
 except PermissionError:
-    os.kill(service.process.pid, signal.SIGTERM)
+    os.system(f"sudo kill -9 {service.process.pid}")
