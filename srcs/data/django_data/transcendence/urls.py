637e5de05c30cbaf7d4b9aae23da73f1639b6063
qcoudeyr <qcoudeyr@student.42perpignan.fr>
2024-09-03 16:01:18 +0000

    Tuesday, September 3, 2024 at 6:01:18 PM GMT+02:00

--- srcs/data/django_data/transcendence/urls.py
+++ srcs/data/django_data/transcendence/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
 	path('api/register', views.register, name='register'),
+	path('api/login', views.login_view, name='login'),
 ]
 
--- srcs/data/django_data/transcendence/views.py
+++ srcs/data/django_data/transcendence/views.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth import authenticate, login
 
 @csrf_exempt
 def register(request):
@@ -27,5 +28,20 @@ def register(request):
         )
 
         return JsonResponse({'message': 'Inscription réussie'})
+    else:
+        return JsonResponse({'error': 'Méthode non autorisée'}, status=405)
+
+@csrf_exempt
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return JsonResponse({'message': 'Connexion réussie'})
+        else:
+            return JsonResponse({'error': 'Nom d\'utilisateur ou mot de passe incorrect'}, status=400)
     else:
         return JsonResponse({'error': 'Méthode non autorisée'}, status=405)
