4b66aca6591022abb40b3268763caf3d55d1a283
qcoudeyr <qcoudeyr@student.42perpignan.fr>
2024-09-12 10:50:29 +0200

    Refactor JavaScript and CSS files for the FrontEnd
    This commit removes unnecessary code from the login.js, loader.css, tournament.css, script.js, and single_page.js files in the FrontEnd directory. The removed code includes functions for switching forms, a loader animation, tournament styling, audio playback, and navigation between sections. These changes improve the codebase by eliminating unused code and reducing complexity.

--- srcs/requirements/nodejs/FrontEnd/JsFiles/login.js
+++
@@ -1,9 +0,0 @@
-function switchForm(event) {
-	event.preventDefault();
-	const loginForm = document.querySelector('.login-form');
-	const registerForm = document.querySelector('.register-form');
-	loginForm.classList.toggle('fade-out');
-	loginForm.classList.toggle('fade-in');
-	registerForm.classList.toggle('fade-out');
-	registerForm.classList.toggle('fade-in');
-}
