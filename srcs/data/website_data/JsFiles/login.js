fd0b18dcbb761e8f1a55c693c00b65f30fbde85f
qcoudeyr <qcoudeyr@student.42perpignan.fr>
2024-09-13 19:19:47 +0200

    abort logto as auth portal

--- srcs/data/website_data/JsFiles/login.js
+++ srcs/data/website_data/JsFiles/login.js
@@ -1,25 +1,3 @@
-import LogtoClient from '@logto/browser';
-
-const logtoClient = new LogtoClient({
-  endpoint: 'https://qywu8v.logto.app/',
-  appId: 's699oq36lcet7unczsad9',
-});
-
-const isAuthenticated = await logtoClient.isAuthenticated();
-
-const onClickSignIn = () => {
-  logtoClient.signIn('http://localhost:3000/');
-};
-const onClickSignOut = () => {
-  logtoClient.signOut('http://localhost:3000/');
-};
-
-const button = document.createElement('button');
-button.innerHTML = isAuthenticated ? 'Sign Out' : 'Sign In';
-button.addEventListener('click', isAuthenticated ? onClickSignOut : onClickSignIn);
-
-document.body.appendChild(button);
-
 function switchForm(event) {
 	event.preventDefault();
 	const loginForm = document.querySelector('.login-form');
