c082063d19faa891625027d30485fb57946c5443
gdornic <gdornic@student.42perpignan.fr>
2024-09-17 10:08:52 +0200

    Admin page available on localhost:8000

--- srcs/data/django_data/transcendence/transcendence/urls.py
+++ srcs/data/django_data/transcendence/transcendence/urls.py
@@ -1,7 +1,8 @@
-"""transcendence URL Configuration
+"""
+URL configuration for transcendence project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,10 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
 
 urlpatterns = [
-	path('api/register', views.register, name='register'),
-	path('api/login', views.login_view, name='login'),
+    path('admin/', admin.site.urls),
 ]
-
