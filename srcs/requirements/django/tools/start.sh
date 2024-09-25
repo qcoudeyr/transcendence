917986dc48f6c10cb41f252d9315162ff42df975
gdornic <gdornic@student.42perpignan.fr>
2024-09-25 12:51:30 +0200

    migrations reset when restarting django container

--- srcs/requirements/django/tools/start.sh
+++ srcs/requirements/django/tools/start.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-python manage.py makemigrations
+apps=(
+    users
+    profiles
+)
+for i in "${apps[@]}"; do
+    rm -rf "$i"/migrations
+done
+for i in "${apps[@]}"; do
+    python manage.py makemigrations "$i"
+done
+
 python manage.py migrate
 
 python manage.py createsuperuser --noinput --username "$DJANGO_SUPERUSER_NAME" --email "$DJANGO_SUPERUSER_EMAIL"
