9d0f25d1c272f1c4eafede5ba92413e3d1d4b873
Luke <lumontgo@student.42perpignan.fr>
2024-10-27 15:02:42 +0100

    Update 3d.js

--- srcs/data/website_data/JsFiles/3d/3d.js
+++ srcs/data/website_data/JsFiles/3d/3d.js
@@ -33,7 +33,7 @@ function initScene() {
 
   // Create the scene
   scene = new THREE.Scene();
-  ball.position.y = 0.1;
+  ball.position.y = 0.15;
 
 
   const ambientLight = new THREE.AmbientLight(0xffffff, 0.1); // Bright white light, intensity 1.5
