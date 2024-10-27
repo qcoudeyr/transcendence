46e6ee7e7a870cbf5e13c9b11eacf95cdf3c456f
RussellParadox <gdornic.pro@gmail.com>
2024-10-28 00:52:11 +0100

    trying to improve render

--- srcs/data/channels_data/transcendence/events/tasks.py
+++ srcs/data/channels_data/transcendence/events/tasks.py
@@ -131,7 +131,7 @@ def classic_game(player_ids):
             players_send_object(player_ids, pad_0, 'PAD_0')
             players_send_object(player_ids, pad_1, 'PAD_1')
 
-            time.sleep(0.001)
+            time.sleep(0.005)
 
     # Send game results (as frame message ?)
     # Update profiles status and player things (is_game_ready, movement...)
