abcb9c285886bb0c7ebdb5a3f4b9b51bf44072f9
RussellParadox <gdornic.pro@gmail.com>
2024-10-30 17:54:25 +0100

    fix channel_layer missing in update

--- srcs/data/channels_data/transcendence/events/workers/update.py
+++ srcs/data/channels_data/transcendence/events/workers/update.py
@@ -1,4 +1,7 @@
 from channels.consumer import AsyncConsumer
+from channels.layers import get_channel_layer
+
+channel_layer = get_channel_layer()
 
 class UpdateConsumer(AsyncConsumer):
     async def game_update(self, event):
