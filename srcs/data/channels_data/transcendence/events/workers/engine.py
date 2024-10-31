58edcc6377eba9e85a18c184ded9aaf1e4eab7cd
RussellParadox <gdornic.pro@gmail.com>
2024-10-31 03:19:28 +0100

    create_task without await

--- srcs/data/channels_data/transcendence/events/workers/engine.py
+++ srcs/data/channels_data/transcendence/events/workers/engine.py
@@ -148,6 +148,6 @@ class EngineConsumer(AsyncConsumer):
         # Create game instance
         try:
             engine = PongEngine(game_id, player_ids)
-            await asyncio.create_task(engine.game_loop())
+            asyncio.create_task(engine.game_loop())
         except Exception:
             logging.error(traceback.format_exc())
