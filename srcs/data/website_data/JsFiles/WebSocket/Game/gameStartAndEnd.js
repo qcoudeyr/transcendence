1a98e904e3c38298694d86f7bea60d00ac42edf1
Luke <lumontgo@student.42perpignan.fr>
2024-11-02 00:35:58 +0100

    Update gameStartAndEnd.js

--- srcs/data/website_data/JsFiles/WebSocket/Game/gameStartAndEnd.js
+++ srcs/data/website_data/JsFiles/WebSocket/Game/gameStartAndEnd.js
@@ -2,24 +2,23 @@
 export function gameReadySoStart()
 {
 
-function handleSocketMessage(event) {
-    const data = JSON.parse(event.data);
+document.addEventListener("keydown", handleKeyPress);
+// function handleSocketMessage(event) {
+//     const data = JSON.parse(event.data);
 
-    if (data.type === "game_start") {
-        // Set up key press listener when the game starts
-        document.addEventListener("keydown", handleKeyPress);
-    } else if (data.type === "game_end") {
-        // Remove key press listener when the game ends
-        document.removeEventListener("keydown", handleKeyPress);
-    }
-}
+//     if (data.type === "game_start") {
+//         // Set up key press listener when the game starts
+//         document.addEventListener("keydown", handleKeyPress);
+//     } else if (data.type === "game_end") {
+//         // Remove key press listener when the game ends
+//         document.removeEventListener("keydown", handleKeyPress);
+//     }
+// }
 
-function handleKeyPress(event) {
-    if (event.key === 'D' || event.key === 'd') {
-        sendGameMove("right");
-    } else if (event.key === 'A' || event.key === 'a') {
-        sendGameMove("left");
-    }
+
+
+
+	//start the game
 }
 
 function sendGameMove(direction) {
@@ -29,8 +28,6 @@ function sendGameMove(direction) {
     };
     socket.send(JSON.stringify(message));
 }
-	//start the game
-}
 
 export function gameEnd()
 {
@@ -38,3 +35,10 @@ export function gameEnd()
 	//show frame you lost or you won then make the button leave apear on top
 }
 
+function handleKeyPress(event) {
+    if (event.key === 'D' || event.key === 'd') {
+        sendGameMove("right");
+    } else if (event.key === 'A' || event.key === 'a') {
+        sendGameMove("left");
+    }
+}
