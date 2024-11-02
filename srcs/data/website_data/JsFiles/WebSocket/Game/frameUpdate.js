fec7e56f69e61e792857a5971f296f5b9a243b48
Luke <lumontgo@student.42perpignan.fr>
2024-11-02 12:39:03 +0100

    Timer in scoreboard working

--- srcs/data/website_data/JsFiles/WebSocket/Game/frameUpdate.js
+++ srcs/data/website_data/JsFiles/WebSocket/Game/frameUpdate.js
@@ -3,3 +3,5 @@ export function frameUpdate(textContent)
 	frame = document.getElementById("frame");
 	frame.textContent = textContent;
 }
+
+
--- srcs/data/website_data/JsFiles/WebSocket/Game/updateScore.js
+++ srcs/data/website_data/JsFiles/WebSocket/Game/updateScore.js
@@ -7,3 +7,19 @@ export function scoreUpdate(score1, score2)
 		score1Element.textContent = score1;
 		score2Element.textContent = score2;
 }
+
+export function scoreboardFill(data)
+{
+	if (data && data.TIMER) {
+        // Get the ball mesh from your existing function
+        const time = Document.getElementById("scoreboard-timer")
+        time.textContent = data.TIMER.minutes + ":" + data.TIMER.secondes;
+	}
+	if (data && data.SCORE) {
+		const scoreleft = document.getElementById("scoreboard-score-left");
+		const scoreright = document.getElementById("scoreboard-score-right");
+		scoreright.textContent = data.SCORE.right;
+		scoreleft.textContent = data.SCORE.left;
+	} 
+	
+}
