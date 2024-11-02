44f06fbe0a226e2d034b5a5bdc905e9ff2ba85b8
Luke <lumontgo@student.42perpignan.fr>
2024-11-02 21:44:58 +0100

    Update Statistiques.js

--- srcs/data/website_data/JsFiles/WebSocket/Statistiques.js
+++ srcs/data/website_data/JsFiles/WebSocket/Statistiques.js
@@ -26,67 +26,76 @@ export function fillStats(data)
 let hoursPlayedChart;
 
 export function createHoursPlayedChart(chartData) {
-    const ctx = document.getElementById('hoursPlayedChart').getContext('2d');
 
-    // Destroy the existing chart if it already exists
-    if (hoursPlayedChart) {
-        hoursPlayedChart.destroy();
-    }
+	if (chartData.hours_played)
+	{
+		const ctx = document.getElementById('hoursPlayedChart').getContext('2d');
 
-    // Create a new chart instance
-    hoursPlayedChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-            datasets: [{
-                label: 'Hours Played',
-                data: [
-                    chartData.hours_played.jan,
-                    chartData.hours_played.feb,
-                    chartData.hours_played.mar,
-                    chartData.hours_played.apr,
-                    chartData.hours_played.may,
-                    chartData.hours_played.jun,
-                    chartData.hours_played.jul,
-                    chartData.hours_played.aug,
-                    chartData.hours_played.sep,
-                    chartData.hours_played.oct,
-                    chartData.hours_played.nov,
-                    chartData.hours_played.dec
-                ],
-                backgroundColor: 'rgba(255, 0, 255, 0.6)',
-                borderColor: '#ff00ff',
-                borderWidth: 1,
-                hoverBackgroundColor: 'rgb(0, 255, 255)',
-                hoverBorderColor: '#ff00ff'
-            }]
-        },
-        options: {
-            responsive: true,
-            scales: {
-                y: {
-                    beginAtZero: true,
-                    grid: {
-                        color: 'rgba(255, 0, 255, 0.6)'
-                    },
-                    ticks: {
-                        color: '#ff00ff'
-                    }
-                },
-                x: {
-                    grid: {
-                        color: 'rgba(255, 0, 255, 0.6)'
-                    },
-                    ticks: {
-                        color: '#ff00ff'
-                    }
-                }
-            },
-            plugins: {
-                legend: {
-                    display: false
-                }
-            }
-        }
-    });
+		// Destroy the existing chart if it already exists
+		if (hoursPlayedChart) {
+			hoursPlayedChart.destroy();
+		}
+	
+		// Create a new chart instance
+		hoursPlayedChart = new Chart(ctx, {
+			type: 'bar',
+			data: {
+				labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+				datasets: [{
+					label: 'Hours Played',
+					data: [
+						chartData.hours_played.jan,
+						chartData.hours_played.feb,
+						chartData.hours_played.mar,
+						chartData.hours_played.apr,
+						chartData.hours_played.may,
+						chartData.hours_played.jun,
+						chartData.hours_played.jul,
+						chartData.hours_played.aug,
+						chartData.hours_played.sep,
+						chartData.hours_played.oct,
+						chartData.hours_played.nov,
+						chartData.hours_played.dec
+					],
+					backgroundColor: 'rgba(255, 0, 255, 0.6)',
+					borderColor: '#ff00ff',
+					borderWidth: 1,
+					hoverBackgroundColor: 'rgb(0, 255, 255)',
+					hoverBorderColor: '#ff00ff'
+				}]
+			},
+			options: {
+				responsive: true,
+				scales: {
+					y: {
+						beginAtZero: true,
+						grid: {
+							color: 'rgba(255, 0, 255, 0.6)'
+						},
+						ticks: {
+							color: '#ff00ff'
+						}
+					},
+					x: {
+						grid: {
+							color: 'rgba(255, 0, 255, 0.6)'
+						},
+						ticks: {
+							color: '#ff00ff'
+						}
+					}
+				},
+				plugins: {
+					legend: {
+						display: false
+					}
+				}
+			}
+		});
+	}
+	else
+	{
+		console.log("Currently no hours played.");
+	}
+    
 }
