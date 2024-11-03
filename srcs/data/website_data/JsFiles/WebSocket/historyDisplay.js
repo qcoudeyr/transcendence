bf65953fa45961f55e5607dfe12a8f1ff1296b03
Luke <lumontgo@student.42perpignan.fr>
2024-11-03 01:32:54 +0100

    Update historyDisplay.js

--- srcs/data/website_data/JsFiles/WebSocket/historyDisplay.js
+++ srcs/data/website_data/JsFiles/WebSocket/historyDisplay.js
@@ -5,14 +5,15 @@ export function createMatchHistory(data) {
 	const historyMatchContainer = document.getElementById("match-history-container");
 
     const matchDiv = document.createElement('div');
-    matchDiv.id = data.id;
+    matchDiv.id = data.history_id;
     if (data.result === "LOSE")
         matchDiv.classList.add("game", "lost");
     else
         matchDiv.classList.add("game", "won");
-		matchDiv.setAttribute('onclick', `selectMatch('${data.result}', '${data.score.left + '-' + data.score.right}')`);
+
+	matchDiv.setAttribute('onclick', `selectMatch('${data.result}', '${data.score.left + '-' + data.score.right}')`);
     const matchText = document.createElement('p');
-    matchText.textContent = 'Match ' + data.id;
+    matchText.textContent = 'Match ' + data.history_id;
     matchDiv.appendChild(matchText);
     historyMatchContainer.appendChild(matchDiv);
 }
