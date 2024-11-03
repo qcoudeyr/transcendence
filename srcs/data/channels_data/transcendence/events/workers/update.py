6004e902aa60c453131999003cbc7104ea6a6ad5
RussellParadox <gdornic.pro@gmail.com>
2024-11-03 14:27:07 +0100

    to test

--- srcs/data/channels_data/transcendence/events/workers/update.py
+++ srcs/data/channels_data/transcendence/events/workers/update.py
@@ -9,30 +9,31 @@ channel_layer = get_channel_layer()
 
 TICK_RATE = 1.0 / 64
 
+async def update_loop(game_channel):
+    # Tick rate setup
+    tick_rate = TICK_RATE
+    tick_count = 0
+    start_time = time.time()
+
+    update_continue = True
+    while (update_continue):
+        # Retrieve and send game state
+        game_state = await cache.aget(game_channel)
+        await channel_layer.group_send(
+            game_channel,
+            game_state
+        )
+        if game_state['ENDED']:
+            update_continue = False
+
+        # Ensure tick rate
+        tick_count += 1
+        targeted_time = start_time + tick_rate * tick_count
+        time_to_wait = targeted_time - time.time()
+        if time_to_wait > 0:
+            await asyncio.sleep(time_to_wait)
+
 class UpdateConsumer(AsyncConsumer):
-    async def update_loop(self):
-        # Tick rate setup
-        tick_rate = TICK_RATE
-        tick_count = 0
-        start_time = time.time()
-
-        update_continue = True
-        while (update_continue):
-            # Retrieve and send game state
-            game_state = await cache.aget(self.game_channel)
-            await channel_layer.group_send(
-                self.game_channel,
-                game_state
-            )
-            if game_state['ENDED']:
-                update_continue = False
-
-            # Ensure tick rate
-            tick_count += 1
-            targeted_time = start_time + tick_rate * tick_count
-            time_to_wait = targeted_time - time.time()
-            if time_to_wait > 0:
-                await asyncio.sleep(time_to_wait)
 
     async def game_update(self, event):
         await channel_layer.group_send(
@@ -44,7 +45,7 @@ class UpdateConsumer(AsyncConsumer):
             }
         )
         if 'game_channel' in event:
-            self.game_channel = event['game_channel']
+            game_channel = event['game_channel']
         else:
             return
-        asyncio.create_task(self.update_loop())
+        asyncio.create_task(update_loop(game_channel))
